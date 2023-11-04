4b1e60b638840c550c7573c8f68ac9aafa2f7c72
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2023-11-04 09:47:36 +0100

    j3x3g: We don't need telephony

--- omni_j3x3g.mk
+++ omni_j3x3g.mk
@@ -1,9 +1,6 @@
 # Release name
 PRODUCT_RELEASE_NAME := j3x3g
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
