bcc7396446f53edf319959e20ce607dbcc68ecdf
NotNoelChannel <notnoel12345678@gmail.com>
2024-11-19 19:02:10 +0100

    cm-14.1: changes changes changes

--- lineage.mk
+++ lineage.mk
@@ -1,22 +1,18 @@
-# Release name
-PRODUCT_RELEASE_NAME := gtexslte
-
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, device/samsung/gtexslte/device_gtexslte.mk)
+# Inherit from j3x3g device
+$(call inherit-product, device/samsung/j3x3g/device.mk)
 
-# Use specific resolution for bootanimation
-TARGET_BOOTANIMATION_SIZE := 720x480
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := gtexslte
-PRODUCT_NAME := lineage_gtexslte
+PRODUCT_DEVICE := j3x3g
+PRODUCT_NAME := lineage_j3x3g
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T285
+PRODUCT_MODEL := SM-J320H
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := tablet
 
-TARGET_SCREEN_WIDTH := 800
-TARGET_SCREEN_HEIGHT := 1280
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=samsung/j3x3gxx/j3x3g:5.1.1/LMY47V/J320HXXU0AQK1:user/release-keys \
+    PRIVATE_BUILD_DESC="j3x3gxx-user 5.1.1 LMY47V J320HXXU0AQK1 release-keys"
--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,27 +1,49 @@
 LOCAL_PATH := $(call my-dir)
 
-MKBOOTIMG_TOOL :=  $(HOST_OUT_EXECUTABLES)/degas-mkbootimg
-DTBTOOL := 	./scripts/mkdtimg.sh
-INSTALLED_DTIMAGE_TARGET := $(TOP)/device/samsung/gtexslte/dt.img
-INSTALLED_SEANDROID_IMAGE := $(TOP)/device/samsung/gtexslte/seandroid.img
+## Build and run dtbtool
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+DTBTAGNAME := "sprd,sc-id = <"
 
-# $(INSTALLED_DTIMAGE_TARGET): $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-# 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
-# 	$(call pretty,"Target dt image: ${INSTALLED_DTIMAGE_TARGET}")
-# 	$(hide) cd $(TOP)/kernel/samsung/gtexslte && $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -p $(KERNEL_OUT) -i $(TOP)/kernel/samsung/gtexslte/arch/arm/boot/dts/
-# 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+INSHEAD := $(LOCAL_PATH)/insertheader/prebuilt/bin/imgheaderinsert
+SIGNEDBIMG ?= $(PRODUCT_OUT)/boot-sign.img
+SIGNEDRIMG ?= $(PRODUCT_OUT)/recovery-sign.img
+SPRDSIGN := $(LOCAL_PATH)/signimage/prebuilt/bin/sprd_sign
+SPRDSIGNCFG := $(LOCAL_PATH)/signimage/config
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG_TOOL) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) $(INSTALLED_SEANDROID_IMAGE)
-	$(call pretty,"Target boot image: $@  ${MKBOOTIMG_TOOL} ${INTERNAL_BOOTIMAGE_ARGS} ${BOARD_MKBOOTIMG_ARGS}")
-	$(hide) $(MKBOOTIMG_TOOL) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --signature $(INSTALLED_SEANDROID_IMAGE) --output $@
+ifneq ($(TARGET_KERNEL_ARCH),)
+KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
+else
+KERNEL_ARCH := $(TARGET_ARCH)
+endif
+
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
+	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+
+# Overload bootimg generation:
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTFS) -d $(TARGET_OUT) $(TARGET_ROOT_OUT) | $(BOOT_RAMDISK_COMPRESSOR) > $(INSTALLED_RAMDISK_TARGET)
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	@echo "SEANDROIDENFORCE" >> $@
+	$(hide) $(INSHEAD) $(INSTALLED_BOOTIMAGE_TARGET) 1
+	$(hide) mv $(SIGNEDBIMG) $(INSTALLED_BOOTIMAGE_TARGET)
+	$(hide) $(SPRDSIGN) $(INSTALLED_BOOTIMAGE_TARGET) $(SPRDSIGNCFG)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
-## Overload recoveryimg generation: Same as the original, + --dt arg
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG_TOOL) $(INSTALLED_DTIMAGE_TARGET) \
+## Overload recoveryimg generation:
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
-	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG_TOOL) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call build-recoveryimage-target, $@)
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) echo -n "SEANDROIDENFORCE" >> $(INSTALLED_RECOVERYIMAGE_TARGET)
+	$(hide) $(INSHEAD) $(INSTALLED_RECOVERYIMAGE_TARGET) 1
+	$(hide) mv $(SIGNEDRIMG) $(INSTALLED_RECOVERYIMAGE_TARGET)
+	$(hide) $(SPRDSIGN) $(INSTALLED_RECOVERYIMAGE_TARGET) $(SPRDSIGNCFG)
+	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(PRODUCT_OUT)/recovery.tar
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
